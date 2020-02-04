0c60c39fa0efdcfc3f7620948b257ae4adda9696
Oleksii Romanko <o.roman'ko@relevant.software>
2020-02-04 17:50:34 +0200

    Test prjct add button add foreground state trigger

--- WindowAbove/Test/TestFrmwrk/TestFrmwrk/SceneDelegate.swift
+++ WindowAbove/Test/TestFrmwrk/TestFrmwrk/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ADFramework
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -38,8 +39,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+          let deadlineTime = DispatchTime.now() + .milliseconds(250)
+                    DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
+                         let ad: ADViewModel = ADViewModel(params: "app foreground iOS 13", "bottom", "p3", "p4", "p5",callbackOpen: {
+                        }) {  (val1, val2, val3) in
+
+                        }
+                        //ADPresenter.shared.initADEachTime = false // if false - don't reload page each time
+                        ADPresenter.shared.showAD(viewModel: ad)
+                    }
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
--- WindowAbove/Test/TestFrmwrk/TestFrmwrk/ViewController.swift
+++ WindowAbove/Test/TestFrmwrk/TestFrmwrk/ViewController.swift
@@ -13,16 +13,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-          let deadlineTime = DispatchTime.now() + .milliseconds(250)
-              DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
-                   let ad: ADViewModel = ADViewModel(params: "app foreground iOS 13", "bottom", "p3", "p4", "p5",callbackOpen: {
-                  }) {  (val1, val2, val3) in
+    }
 
-                  }
-                  //ADPresenter.shared.initADEachTime = false // if false - don't reload page each time
-                  ADPresenter.shared.showAD(viewModel: ad)
-              }
+    @IBAction func btnOpenClick(_ sender: Any) {
+         let ad: ADViewModel = ADViewModel(params: "button click", "bottom", "p3", "p4", "p5",callbackOpen: {
+            print ("opened")
+        }) {  (val1, val2, val3) in
+            print ("closed")
+            print (val1)
+            print (val2)
+            print (val3)
+        }
+        //ADPresenter.shared.initADEachTime = false // if false - don't reload page each time
+        ADPresenter.shared.showAD(viewModel: ad)
+
     }
 
 
